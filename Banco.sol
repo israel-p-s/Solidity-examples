558e113ef024afa54f704b81a20d88eab0ffd220
Israel Palma Sebastià <38512074+israel-p-s@users.noreply.github.com>
2021-08-01 00:44:31 +0200

    Update Banco.sol

--- Banco.sol
+++ Banco.sol
@@ -2,9 +2,31 @@ pragma solidity ^0.4.17;
 
 contract Banco {
     
+    // Variable de estado
+    address owner;
+    
+    // Modificador que comprueba que ejecuta el owner
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    // Modificar el owner
+    function newOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+    
+    function getOwner () view public returns (address) {
+        return owner;
+    }
+    
+    function getBalance() view public returns(uint256) {
+        return address(this).balance;
+    }
+    
     // Inicializador del smart contract
     function Banco() payable public {
-        
+        owner = msg.sender;
     }
     
     // Enviar saldo a una dirección
@@ -12,8 +34,8 @@ contract Banco {
         require(msg.value == amount);
     }
     
-    // Recibir y actualizar saldo a la dirección
-    function getBalance() public {
+    // Recibir saldo a la dirección
+    function withdrawBalance() public onlyOwner {
         msg.sender.transfer(address(this).balance);
     }
 }
